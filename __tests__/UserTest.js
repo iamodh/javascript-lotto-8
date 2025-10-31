2f6aae55b1fd029e4dcb24af569529db80c83405
Donghwan Oh <imodh97@gmail.com>
2025-10-31 14:10:54 +0900

    test(user): 사용자 클래스 테스트 작성

---
+++ __tests__/UserTest.js
@@ -0,0 +1,21 @@
+import LOTTO_CONFIG from '../src/constants/lottoConfig';
+import User from '../src/models/User';
+
+describe('사용자 클래스 테스트', () => {
+  test('투자 금액이 로또의 가격으로 나누어 떨어지지 않으면 예외가 발생한다.', () => {
+    expect(() => {
+      new User(3200);
+    }).toThrow('[ERROR]');
+  });
+
+  test('투자 금액에 맞는 개수의 로또를 구입한다.', () => {
+    const investment = 3000;
+    const quantity = investment / LOTTO_CONFIG.PRICE;
+
+    const user = new User(investment);
+    user.purchaseLottos(quantity);
+    const purchasedLottos = user.getPurchasedLottos();
+
+    expect(purchasedLottos.length).toBe(quantity);
+  });
+});
