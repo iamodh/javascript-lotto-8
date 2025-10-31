e0dbaed39676d92f18ad9eb98b46696ee81df20c
Donghwan Oh <imodh97@gmail.com>
2025-10-31 11:47:15 +0900

    feat(lotto): 로또 클래스 기능 구현

---
+++ src/models/Lotto.js
@@ -0,0 +1,24 @@
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validateCount(numbers);
+    this.#validateDuplicates(numbers);
+    this.#numbers = numbers;
+  }
+
+  #validateCount(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+  }
+
+  #validateDuplicates(numbers) {
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error('[ERROR] 로또 번호는 중복이 불가합니다.');
+    }
+  }
+}
+
+export default Lotto;
