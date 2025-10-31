86505e72f0c5049b8d828b2798a412cdc3d1cb8c
Donghwan Oh <imodh97@gmail.com>
2025-10-31 18:57:43 +0900

    docs(input): 입력 클래스 테스트 작성

---
+++ __tests__/InputTest.js
@@ -0,0 +1,82 @@
+import LOTTO_CONFIG from '../src/constants/lottoConfig';
+import { mockQuestions } from '../src/utils/mocks';
+import Input from '../src/views/Input';
+
+describe('입력 클래스 테스트', () => {
+  test('소지 금액을 입력 받아 숫자로 변환 후 반환한다.', async () => {
+    mockQuestions(['3000']);
+
+    const input = new Input();
+    const investment = await input.getInvestmentInput();
+
+    expect(investment).toBe(3000);
+  });
+
+  test('입력된 소지 금액이 양의 정수가 아니라면 예외가 발생한다.', () => {
+    mockQuestions(['0']);
+
+    const input = new Input();
+
+    expect(async () => {
+      await input.getInvestmentInput();
+    }).toThrow('[ERROR]');
+  });
+
+  test('당첨 로또의 번호들을 입력 받아 숫자 배열로 변환 후 반환한다.', async () => {
+    mockQuestions(['1,2,3,4,5,6']);
+
+    const input = new Input();
+    const numbers = await input.getWinningLottoNumbers();
+
+    expect(numbers).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  test('입력된 당첨 로또 번호 중 양의 정수가 아닌 번호가 있다면 예외가 발생한다.', () => {
+    mockQuestions(['1,2,3,4,5,0']);
+
+    const input = new Input();
+
+    expect(async () => {
+      await input.getWinningLottoNumbers();
+    }).toThrow('[ERROR]');
+  });
+
+  test('입력된 당첨 로또 번호 중 로또 번호의 범위에 포함되지 않는 번호가 있다면 예외가 발생한다.', () => {
+    const NUMBER_NOT_IN_RANGE = LOTTO_CONFIG.NUMBER_RANGE_TO + 1;
+    mockQuestions(['1,2,3,4,5,' + NUMBER_NOT_IN_RANGE]);
+
+    const input = new Input();
+
+    expect(async () => {
+      await input.getWinningLottoNumbers();
+    }).toThrow('[ERROR]');
+  });
+
+  test('당첨 로또의 보너스 번호를 입력 받아 변환 후 반환한다.', async () => {
+    mockQuestions(['10']);
+    const input = new Input();
+    const bonusNumber = await input.getBonusNumber();
+
+    expect(bonusNumber).toBe(10);
+  });
+
+  test('당첨 로또의 보너스 번호가 양의 정수가 아니라면 예외가 발생한다.', () => {
+    mockQuestions(['0']);
+    const input = new Input();
+
+    expect(async () => {
+      await input.getBonusNumber();
+    }).toThrow('[ERROR]');
+  });
+
+  test('당첨 로또의 보너스 번호가 로또 번호의 범위에 포함되지 않는다면 예외가 발생한다.', () => {
+    const NUMBER_NOT_IN_RANGE = LOTTO_CONFIG.NUMBER_RANGE_TO + 1;
+
+    mockQuestions([String(NUMBER_NOT_IN_RANGE)]);
+    const input = new Input();
+
+    expect(async () => {
+      await input.getBonusNumber();
+    }).toThrow('[ERROR]');
+  });
+});
