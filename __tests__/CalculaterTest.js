294fd57336851653d1c48bfb465856083e4fdbed
Donghwan Oh <imodh97@gmail.com>
2025-11-01 19:32:27 +0900

    test(calculator): 계산기 클래스 테스트 작성

---
+++ __tests__/CalculaterTest.js
@@ -0,0 +1,23 @@
+import { PRIZE_AMOUNT } from '../src/constants/lottoConfig';
+import Calculator from '../src/models/Calculater';
+
+describe('계산기 클래스 테스트', () => {
+  test('당첨 결과에 따른 수익 금액을 계산하고 수익률을 반환한다.', () => {
+    const INVESTMENT = 8000;
+    const FIRST_FIFTH_PRIZE = [2, 3, 4, 7, 8, 9];
+    const SECOND_FIFTH_PRIZE = [1, 2, 3, 7, 8, 9];
+    const WINNING_RESULT = [
+      [],
+      [],
+      [],
+      [],
+      [FIRST_FIFTH_PRIZE, SECOND_FIFTH_PRIZE],
+    ];
+
+    const PROFIT = PRIZE_AMOUNT[4] * 2;
+    const PROFIT_RATE = ((PROFIT / INVESTMENT) * 100).toFixed(1);
+    const calculator = new Calculator(INVESTMENT, WINNING_RESULT);
+
+    expect(calculator.getProfitRate()).toBe(PROFIT_RATE);
+  });
+});
