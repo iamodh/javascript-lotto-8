dee24a9b6d8e82aae7a68e971dcc96833aafc598
Donghwan Oh <imodh97@gmail.com>
2025-11-02 21:28:35 +0900

    refactor(lotto): 로또 우승 조건을 상수로 분리

--- src/constants/lottoConfig.js
+++ src/constants/lottoConfig.js
@@ -5,4 +5,11 @@ export const LOTTO_CONFIG = Object.freeze({
   NUMBERS_COUNT: 6,
 });
 
+export const PRIZE_CONDITION = [
+  '6개 일치',
+  '5개 일치, 보너스 볼 일치',
+  '5개 일치',
+  '4개 일치',
+  '3개 일치',
+];
 export const PRIZE_AMOUNT = [2000000000, 30000000, 1500000, 50000, 5000];
