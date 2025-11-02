d508e78c9ffcc6fab26830fbed5811e05a0d9276
Donghwan Oh <imodh97@gmail.com>
2025-11-02 21:27:25 +0900

    test(app): 모킹 함수 분리

--- src/utils/mocks.js
+++ src/utils/mocks.js
@@ -9,3 +9,16 @@ export const mockQuestions = (inputs) => {
     return Promise.resolve(input);
   });
 };
+
+export const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+export const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
